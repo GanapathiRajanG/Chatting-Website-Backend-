b889a3b994436aa289904252c4066f36b80bb2b7
GanapathiRajanG <ganapathirajan06@gmail.com>
2025-12-19 11:39:02 +0530

    Updated CORS for Vercel frontend

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://chatting-website-frontend.vercel.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get('/test', (req, res) => {
